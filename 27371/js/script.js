9f70797848e44ebbf5ffbab9eea3c2814a2c08f1
AlexSzczesnowicz <193945w@gmail.com>
2020-11-20 20:46:26 +0100

    27371 - done

--- 27371/js/script.js
+++ 27371/js/script.js
@@ -9,31 +9,45 @@ $(document).ready(function(){
 //$('.paragraf')[2]=$('.paragraf')[4];
 //$('.paragraf').eq(3)=$('.paragraf').eq(1);
 
-$('#knopka1').on('click',function(){
-    Karusel(down);
+$('#upButton').on('click',function(){
+    karusel('up');
     // for(i=0;i<$('.paragraf').length;i++){
     //     let buf1=$('.paragraf').eq(4).text();
    
 });
-function Karusel(direktion){
+$('#downButton').on('click',function(){
+    karusel('down');
+    // for(i=0;i<$('.paragraf').length;i++){
+    //     let buf1=$('.paragraf').eq(4).text();
+   
+});
+
+function karusel(direktion){
     let bufer=[];
+    parStringLen = $('.paragraf').length;
+
     for(i=0;i<$('.paragraf').length;i++){
         bufer[i]=$('.paragraf').eq(i).text();
-        console.log('karusel starts')
+       
+        //console.log('karusel starts')
     }
     switch(direktion){
-        case 'up':
-            break;
-
-
-
-
         case 'down':
-            for(i=1;i<$('.paragraf').length-1;i++){
-                $('.paragraf').eq(i-1).text = bufer[i];
+            for(i=0;i<parStringLen-1;i++){
+                $('.paragraf').eq(i+1).text(bufer[i]);
+                //console.log(bufer[i]);
             }
-                $('.paragraf').eq($('.paragraf').length-1).text = bufer[0];
-                $('.paragraf').eq($('.paragraf').length-2).text = bufer[$('.paragraf').length-1];
+            $('.paragraf').eq(0).text(bufer[parStringLen-1]);
+            $('.paragraf').eq(1).text(bufer[0]);
+
+            break;
+        case 'up':
+            //console.log('down seen');
+            for(i=1;i<parStringLen-1;i++){
+                $('.paragraf').eq(i-1).text(bufer[i]);
+            }
+                $('.paragraf').eq(parStringLen-1).text(bufer[0]);
+                $('.paragraf').eq(parStringLen-2).text(bufer[parStringLen-1]);
 
             break;
             
